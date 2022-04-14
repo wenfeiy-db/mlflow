1555189bbd9265ad637b0dc2419be3af24878e4e
dbczumar <corey.zumar@databricks.com>
2022-04-14 13:19:50 -0700

    Refactor
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

---
+++ mlflow/pipelines/__init__.py
@@ -0,0 +1,117 @@
+import sys
+import shutil
+import subprocess
+import os
+
+import pandas as pd
+import numpy as np
+
+
+def ingest():
+    """
+    Ingest data
+    """
+    _run_ingest(reingest=True)
+
+def split():
+    """
+    Split data
+    """
+    _run_ingest(reingest=False)
+    _run_make("split")
+
+    print("== Showing summary of input data ==\n")
+    _maybe_open("split_summary.html")
+
+    print("Split data into train/test sets")
+
+    print("== Summary of train data ==\n")
+    print(pd.read_parquet("split_train.parquet").describe())
+
+    print("== Summary of test data ==\n")
+    print(pd.read_parquet("split_test.parquet").describe())
+
+
+def transform():
+    """
+    Transform features
+    """
+    _run_ingest(reingest=False)
+    _run_make("transform")
+
+    print("== Summary of transformed features ==\n")
+    df = pd.read_parquet("transform_train_transformed.parquet")
+    X = np.vstack(df["features"])
+    print(pd.DataFrame(X).describe())
+
+
+def train():
+    """
+    Train a model
+    """
+    _run_ingest(reingest=False)
+    _run_make("train")
+
+    print("== Trained a model at train_pipeline.pkl ==\n")
+
+
+def evaluate():
+    """
+    Evaluate a model (explanations included)
+    """
+    _run_ingest(reingest=False)
+    _run_make("evaluate")
+
+    print("== Created the model card ==\n")
+    _maybe_open("evaluate_explanations.html")
+
+    print("== Produced evaluation metrics ==\n")
+    _maybe_open("evaluate_metrics.json")
+
+
+def clean():
+    """
+    Clean
+    """
+    _run_make("clean")
+
+
+def inspect():
+    """
+    Inspect specific steps or full pipeline DAG
+    """
+    raise NotImplementedError
+
+
+def _run_in_subprocess_and_stream_results(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    for c in iter(lambda: process.stdout.read(1), b''):
+        sys.stdout.buffer.write(c)
+
+    process.wait()
+
+    if process.returncode != 0:
+        raise RuntimeError(process.stderr.decode())
+
+
+def _run_make(rule_name):
+    _run_in_subprocess_and_stream_results(["make", rule_name])
+
+
+def _maybe_open(path):
+    assert os.path.exists(path), f"{path} does not exist"
+    if shutil.which("open") is not None:
+        subprocess.run(["open", path])
+    else:
+        print(f"Please open {path} manually.")
+
+
+def _run_ingest(reingest=False):
+    """
+    :param reingest: If `True`, reingest data even if it has already been ingested previously.
+                     If `False`, only ingest data even it has not previously been ingested.
+    """
+    pass
+
+
+def _get_repository_root(
--- mlflow/pipelines/cli.py
+++ mlflow/pipelines/cli.py
@@ -1,5 +1,3 @@
-import sys
-import shutil
 import subprocess
 import os
 
@@ -7,93 +5,43 @@ import click
 import pandas as pd
 import numpy as np
 
+import mlflow.pipelines
 
-def _run_in_subprocess_and_stream_results(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE)
-    for c in iter(lambda: process.stdout.read(1), b''):
-        sys.stdout.buffer.write(c)
-
-    process.wait()
-
-    if process.returncode != 0:
-        raise RuntimeError(process.stderr.decode())
-
-def _run_make(rule_name):
-    _run_in_subprocess_and_stream_results(["make", rule_name])
-
-def _maybe_open(path):
-    assert os.path.exists(path), f"{path} does not exist"
-    if shutil.which("open") is not None:
-        subprocess.run(["open", path])
-    else:
-        print(f"Please open {path} manually.")
 
 @click.group("pipelines")
 def commands():
     pass
 
-@commands.command(help='ingest data')
+@commands.command(help='Ingest data')
 def ingest():
-    _run_ingest(reingest=True)
+    mlflow.pipelines.ingest()
 
 
-def _run_ingest(reingest=False):
-    """
-    :param reingest: If `True`, reingest data even if it has already been ingested previously.
-                     If `False`, only ingest data even it has not previously been ingested.
-    """
-    pass
-
-
-@commands.command(help='split data')
+@commands.command(help='Split data')
 def split():
-    _run_ingest(reingest=False)
-    _run_make("split")
-
-    print("== Showing summary of input data ==\n")
-    _maybe_open("split_summary.html")
-
-    print("Split data into train/test sets")
-
-    print("== Summary of train data ==\n")
-    print(pd.read_parquet("split_train.parquet").describe())
-
-    print("== Summary of test data ==\n")
-    print(pd.read_parquet("split_test.parquet").describe())
+    mlflow.pipelines.split()
 
 
 @commands.command(help="Transform features")
 def transform():
-    _run_ingest(reingest=False)
-    _run_make("transform")
+    mlflow.pipelines.transform()
 
-    print("== Summary of transformed features ==\n")
-    df = pd.read_parquet("transform_train_transformed.parquet")
-    X = np.vstack(df["features"])
-    print(pd.DataFrame(X).describe())
 
 @commands.command(help='Train a model')
 def train():
-    _run_ingest(reingest=False)
-    _run_make("train")
+    mlflow.pipelines.train()
 
-    print("== Trained a model at train_pipeline.pkl ==\n")
 
-@commands.command(help='evaluate a model (explanations included)')
+@commands.command(help='Evaluate a model (explanations included)')
 def evaluate():
-    _run_ingest(reingest=False)
-    _run_make("evaluate")
+    mlflow.pipelines.evaluate()
 
-    print("== Created the model card ==\n")
-    _maybe_open("evaluate_explanations.html")
-
-    print("== Produced evaluation metrics ==\n")
-    _maybe_open("evaluate_metrics.json")
 
 @commands.command(help='Clean')
 def clean():
-    _run_make("clean")
+    mlflow.pipelines.clean()
 
-@commands.command(help='Show DAG')
+
+@commands.command(help='Inspect specific steps or full pipeline DAG')
 def inspect():
-    raise NotImplemented
+    mlflow.pipelines.inspect()
