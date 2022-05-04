9cf9c1285db5d8416b6c949014394cd200cd24af
dbczumar <corey.zumar@databricks.com>
2022-05-03 23:45:29 -0700

    Remove unnecessary test
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- tests/pipelines/test_step.py
+++ tests/pipelines/test_step.py
@@ -31,13 +31,3 @@ class TestBaseStep(TestCase):
             step = BaseStepImplemented({BaseStep._TRACKING_URI_CONFIG_KEY: URI}, "")
             step.run("")
             set_tracking_uri.assert_called_once_with(URI)
-
-    def test_from_step_config_path(self):
-        step_config_path = "step_config_path"
-        pipeline_root = "pipeline_root"
-        with patch("mlflow.utils.file_utils.read_yaml") as read_yaml:
-            baseStepInstance = BaseStepImplemented.from_step_config_path(
-                step_config_path, pipeline_root
-            )
-            assert baseStepInstance.step_config == read_yaml.return_value
-            read_yaml.assert_called_once_with(os.getcwd(), step_config_path)
