c449d4982d043f1c2c05ffc9fd290aac96689c62
dbczumar <corey.zumar@databricks.com>
2022-05-06 02:52:01 -0700

    Testing
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

---
+++ mlflow/pipelines/regression/v1/steps/ingest/__init__.py
@@ -0,0 +1,178 @@
+import logging
+import os
+from abc import abstractmethod
+
+from mlflow.exceptions import MlflowException
+from mlflow.pipelines.step import BaseStep
+from mlflow.pipelines.cards import IngestCard
+from mlflow.protos.databricks_pb2 import INVALID_PARAMETER_VALUE, INTERNAL_ERROR
+from mlflow.utils.file_utils import read_parquet_as_pandas_df
+from mlflow.pipelines.regression.v1.steps.ingest.datasets import (
+    _ParquetDataset,
+    _DeltaTableDataset,
+    _SparkSqlDataset,
+    _CustomDataset,
+)
+
+_logger = logging.getLogger(__name__)
+
+
+class IngestStep(BaseStep):
+    _DATASET_FORMAT_SPARK_TABLE = "spark_table"
+    _DATASET_FORMAT_DELTA = "delta"
+    _DATASET_FORMAT_PARQUET = "parquet"
+    _DATASET_OUTPUT_NAME = "dataset.parquet"
+    _STEP_CARD_OUTPUT_NAME = "card.html"
+    _SUPPORTED_DATASETS = [
+        _ParquetDataset,
+        _DeltaTableDataset,
+        _SparkSqlDataset,
+        # NB: The custom dataset is deliberately listed last as a catch-all for any
+        # format not matched by the datasets above. When mapping a format to a dataset,
+        # datasets are explored in the listed order
+        _CustomDataset,
+    ]
+
+    def __init__(self, step_config, pipeline_root):
+        super().__init__(step_config, pipeline_root)
+
+        dataset_format = step_config.get("format")
+        if not dataset_format:
+            raise MlflowException(
+                message="TODO: NEED FORMAT",
+                error_code=INVALID_PARAMETER_VALUE,
+            )
+
+        for dataset_class in IngestStep._SUPPORTED_DATASETS:
+            if dataset_class.matches_format(dataset_format):
+                self.dataset = dataset_class.from_config(
+                    config=step_config,
+                    pipeline_root=pipeline_root,
+                )
+                break
+        else:
+            raise MlflowException(
+                "TODO: UNRECOGNIZED FORMAT CATCH-ALL",
+                error_code=INVALID_PARAMETER_VALUE,
+            )
+
+    def _run(self, output_directory):
+        dataset_dst_path = os.path.abspath(
+            os.path.join(output_directory, IngestStep._DATASET_OUTPUT_NAME)
+        )
+        self.dataset.resolve_to_parquet(dst_path=dataset_dst_path)
+        _logger.info("Successfully stored data in parquet format at '%s'", dataset_dst_path)
+
+        step_card = IngestStep._build_step_card(
+            ingested_parquet_dataset_path=dataset_dst_path,
+            dataset_src_location=getattr(self.dataset, "location", None),
+            dataset_sql=getattr(self.dataset, "sql", None),
+        )
+        with open(os.path.join(output_directory, IngestStep._STEP_CARD_OUTPUT_NAME), "w") as f:
+            f.write(step_card.to_html())
+
+    @staticmethod
+    def _build_step_card(ingested_parquet_dataset_path, dataset_src_location=None, dataset_sql=None):
+        if dataset_src_location is None and dataset_sql is None:
+            raise MlflowException("TODO: MUST SPECIFY ONE", error_code=INTERNAL_ERROR)
+
+        dataset_df = read_parquet_as_pandas_df(data_parquet_path=ingested_parquet_dataset_path)
+        card = IngestCard()
+        card.add_markdown(
+            name="DATASET_SOURCE",
+            markdown=(
+                f"**Dataset source location:** `{dataset_src_location}`"
+                if dataset_src_location is not None else
+                f"**Dataset SQL:** `{dataset_sql}`"
+            ),
+        )
+        card.add_markdown(
+            name="RESOLVED_DATASET_LOCATION",
+            markdown=f"**Ingested dataset path:** `{ingested_parquet_dataset_path}`",
+        )
+        card.add_markdown(
+            name="DATASET_NUM_ROWS",
+            markdown=f"**Number of rows:** {len(dataset_df)}",
+        )
+        dataset_size = IngestStep._get_dataset_size(dataset_path=ingested_parquet_dataset_path)
+        card.add_markdown(
+            name="DATASET_SIZE",
+            markdown=f"**Size:** {dataset_size}",
+        )
+        dataset_types = dataset_df.dtypes.to_frame(name="column type").transpose()
+        dataset_types = dataset_types.reset_index(level=0)
+        dataset_types = dataset_types.rename(columns={"index": "column name"})
+        dataset_types_styler = (
+            dataset_types.style.set_properties(**{"text-align": "center"})
+            .hide(axis="index")
+            .set_table_styles(
+                [
+                    {"selector": "", "props": [("border", "1px solid grey")]},
+                    {"selector": "tbody td", "props": [("border", "1px solid grey")]},
+                    {"selector": "th", "props": [("border", "1px solid grey")]},
+                ]
+            )
+        )
+        card.add_artifact(
+            name="DATASET_SCHEMA",
+            artifact=dataset_types_styler.to_html(),
+            artifact_format="html",
+        )
+        dataset_sample = dataset_df.sample(n=min(len(dataset_df), 10), random_state=42).sort_index()
+        dataset_sample_styler = (
+            dataset_sample.style.set_properties(**{"text-align": "center"})
+            .format(precision=2)
+            .set_table_styles(
+                [
+                    {"selector": "", "props": [("border", "1px solid grey")]},
+                    {"selector": "tbody td", "props": [("border", "1px solid grey")]},
+                    {"selector": "th", "props": [("border", "1px solid grey")]},
+                ]
+            )
+        )
+        card.add_artifact(
+            name="DATASET_SAMPLE",
+            artifact=dataset_sample_styler.to_html(),
+            artifact_format="html",
+        )
+        return card
+
+    @staticmethod
+    def _get_dataset_size(dataset_path):
+        kb = 10**3
+        mb = 10**6
+        gb = 10**9
+
+        size = os.path.getsize(dataset_path)
+        if size >= gb:
+            return "{:0.2f} GB".format(size / gb)
+        elif size >= mb:
+            return "{:0.2f} MB".format(size / mb)
+        elif size >= kb:
+            return "{:0.2f} KB".format(size / kb)
+        else:
+            return f"{size} B"
+
+    def inspect(self, output_directory):
+        parquet_dataset_path = os.path.join(output_directory, IngestStep._DATASET_OUTPUT_NAME)
+        return IngestStep._build_step_card(
+            dataset_src_location=self.dataset_location,
+            ingested_parquet_dataset_path=parquet_dataset_path,
+        )
+
+    @classmethod
+    def from_pipeline_config(cls, pipeline_config, pipeline_root):
+        if "data" not in pipeline_config:
+            raise MlflowException(
+                message="TODO: NEED DATA",
+                error_code=INVALID_PARAMETER_VALUE,
+            )
+
+        return cls(
+            step_config=pipeline_config["data"],
+            pipeline_root=pipeline_root,
+        )
+
+    @property
+    def name(self):
+        return "ingest"
---
+++ tests/pipelines/test_ingest_step.py
@@ -0,0 +1,145 @@
+import os
+import pytest
+import shutil
+
+import pandas as pd
+from pyspark.sql import SparkSession
+
+from mlflow.pipelines.regression.v1.steps.ingest import IngestStep
+from mlflow.utils.file_utils import TempDir
+
+from tests.pipelines.helper_functions import (
+    enter_pipeline_example_directory,
+)
+
+
+@pytest.fixture
+def enter_ingest_test_pipeline_directory(enter_pipeline_example_directory):
+    pipeline_example_root_path = enter_pipeline_example_directory
+
+    with TempDir(chdr=True) as tmp:
+        ingest_test_pipeline_path = tmp.path("test_ingest_pipeline")
+        shutil.copytree(pipeline_example_root_path, ingest_test_pipeline_path)
+        os.chdir(ingest_test_pipeline_path)
+        yield os.getcwd()
+
+
+@pytest.fixture
+def pandas_df():
+    return pd.DataFrame.from_dict({
+        "A": ["x", "y", "z"],
+        "B": [1, 2, 3],
+        "C": [-9.2, 82.5, 3.40],
+    })
+
+
+@pytest.fixture(scope="module", autouse=True)
+def spark_session():
+    session = SparkSession.builder.master("local[*]").getOrCreate()
+    yield session
+    session.stop()
+
+
+@pytest.fixture()
+def spark_df(spark_session):
+    return spark_session.createDataFrame(
+        [
+            (0, "a b c d e spark", 1.0),
+            (1, "b d", 0.0),
+            (2, "spark f g h", 1.0),
+            (3, "hadoop mapreduce", 0.0),
+        ],
+        ["id", "text", "label"],
+    ).cache()
+
+
+@pytest.mark.parametrize("use_relative_path", [False, True])
+def test_ingests_parquet_successfully(enter_ingest_test_pipeline_directory, use_relative_path, pandas_df, tmp_path):
+    ingest_test_pipeline_root_path = enter_ingest_test_pipeline_directory
+    dataset_path = tmp_path / "df.parquet"
+    pandas_df.to_parquet(dataset_path)
+    if use_relative_path:
+        dataset_path = os.path.relpath(dataset_path)
+
+    IngestStep.from_pipeline_config(
+        pipeline_config = {
+            "data": {
+                "format": "parquet",
+                "location": str(dataset_path),
+            }
+        },
+        pipeline_root=ingest_test_pipeline_root_path,
+    ).run(output_directory=tmp_path)
+
+    reloaded_df = pd.read_parquet(str(tmp_path / "dataset.parquet"))
+    pd.testing.assert_frame_equal(reloaded_df, pandas_df)
+
+
+@pytest.mark.parametrize("use_relative_path", [False, True])
+def test_ingests_csv_successfully(enter_ingest_test_pipeline_directory, use_relative_path, pandas_df, tmp_path):
+    ingest_test_pipeline_root_path = enter_ingest_test_pipeline_directory
+    dataset_path = tmp_path / "df.csv"
+    pandas_df.to_csv(dataset_path, index=False)
+    if use_relative_path:
+        dataset_path = os.path.relpath(dataset_path)
+
+    IngestStep.from_pipeline_config(
+        pipeline_config={
+            "data": {
+                "format": "csv",
+                "location": str(dataset_path),
+                "custom_loader_method": "steps.ingest.load_file_as_dataframe",
+            }
+        },
+        pipeline_root=ingest_test_pipeline_root_path,
+    ).run(output_directory=tmp_path)
+
+    reloaded_df = pd.read_parquet(str(tmp_path / "dataset.parquet"))
+    pd.testing.assert_frame_equal(reloaded_df, pandas_df)
+
+
+def custom_load_file_as_dataframe(file_path, file_format):
+    return pd.read_csv(file_path, sep="#")
+
+
+@pytest.mark.parametrize("use_relative_path", [False, True])
+def test_ingests_custom_format_successfully(enter_ingest_test_pipeline_directory, use_relative_path, pandas_df, tmp_path):
+    ingest_test_pipeline_root_path = enter_ingest_test_pipeline_directory
+    dataset_path = tmp_path / "df.fooformat"
+    pandas_df.to_csv(dataset_path, index=False, sep="#")
+    if use_relative_path:
+        dataset_path = os.path.relpath(dataset_path)
+
+    IngestStep.from_pipeline_config(
+        pipeline_config = {
+            "data": {
+                "format": "fooformat",
+                "location": str(dataset_path),
+                "custom_loader_method": "tests.pipelines.test_ingest_step.custom_load_file_as_dataframe",
+            }
+        },
+        pipeline_root=ingest_test_pipeline_root_path,
+    ).run(output_directory=tmp_path)
+
+    reloaded_df = pd.read_parquet(str(tmp_path / "dataset.parquet"))
+    pd.testing.assert_frame_equal(reloaded_df, pandas_df)
+
+
+def test_ingests_spark_sql_successfully(enter_ingest_test_pipeline_directory, spark_df, tmp_path):
+    ingest_test_pipeline_root_path = enter_ingest_test_pipeline_directory
+    spark_df.write.saveAsTable("test_table")
+
+    IngestStep.from_pipeline_config(
+        pipeline_config = {
+            "data": {
+                "format": "spark_sql",
+                "sql": "SELECT * FROM test_table ORDER BY id",
+            }
+        },
+        pipeline_root=ingest_test_pipeline_root_path,
+    ).run(output_directory=tmp_path)
+
+    reloaded_df = pd.read_parquet(str(tmp_path / "dataset.parquet"))
+    print(reloaded_df)
+    print(spark_df.toPandas())
+    pd.testing.assert_frame_equal(reloaded_df, spark_df.toPandas())
