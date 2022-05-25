bf7d44e8965e777553a2a1b3aac4b72cff802d80
dbczumar <corey.zumar@databricks.com>
2022-05-24 22:36:06 -0700

    Docstring
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/pipelines/utils/tracking.py
+++ mlflow/pipelines/utils/tracking.py
@@ -10,6 +10,22 @@ _logger = logging.getLogger(__name__)
 def set_experiment(
     experiment_id: str = None, experiment_name: str = None, artifact_location: str = None
 ) -> Experiment:
+    """
+    Set the given experiment as the active experiment. The experiment must either be specified by
+    name via ``experiment_name`` or by ID via ``experiment_id``. The experiment name and ID cannot
+    both be specified.
+
+    :param experiment_name: Case sensitive name of the experiment to be activated. If an experiment
+                            with this name does not exist, a new experiment wth this name is
+                            created.
+    :param experiment_id: ID of the experiment to be activated. If an experiment with this ID
+                          does not exist, an exception is thrown.
+    :param artifact_location: The optional artifact location to set when creating the experiment,
+                              if the experiment does not already exist. If the experiment already
+                              exists, ``artifact_location`` is ignored.
+    :return: An instance of :py:class:``mlflow.entities.Experiment`` representing the new active
+             experiment.
+    """
     client = MlflowClient()
     if experiment_name is not None:
         experiment = client.get_experiment_by_name(name=experiment_name)
