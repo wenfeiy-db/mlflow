861a006a3bf7e2e95f7a9270d71418b2c9f1a90a
dbczumar <corey.zumar@databricks.com>
2022-05-26 17:33:31 -0700

    Simplify test
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- tests/tracking/context/test_system_environment_context.py
+++ tests/tracking/context/test_system_environment_context.py
@@ -1,29 +1,13 @@
-import os
-
-import pytest
-
 from mlflow.tracking.context.system_environment_context import SystemEnvironmentContext
 
 
-@pytest.fixture(autouse=True)
-def reset_run_context_env_var():
-    run_context = os.environ.get("MLFLOW_RUN_CONTEXT")
-    try:
-        yield
-    finally:
-        if run_context is not None:
-            os.environ["MLFLOW_RUN_CONTEXT"] = run_context
-        else:
-            os.environ.pop("MLFLOW_RUN_CONTEXT", None)
-
-
-def test_system_environment_context_in_context():
-    os.environ["MLFLOW_RUN_CONTEXT"] = '{"A": "B"}'
+def test_system_environment_context_in_context(monkeypatch):
+    monkeypatch.setenv("MLFLOW_RUN_CONTEXT", '{"A": "B"}')
     assert SystemEnvironmentContext().in_context()
-    del os.environ["MLFLOW_RUN_CONTEXT"]
+    monkeypatch.delenv("MLFLOW_RUN_CONTEXT", raising=True)
     assert not SystemEnvironmentContext().in_context()
 
 
-def test_system_environment_context_tags():
-    os.environ["MLFLOW_RUN_CONTEXT"] = '{"A": "B", "C": "D"}'
+def test_system_environment_context_tags(monkeypatch):
+    monkeypatch.setenv("MLFLOW_RUN_CONTEXT", '{"A": "B", "C": "D"}')
     assert SystemEnvironmentContext().tags() == {"A": "B", "C": "D"}
