c778542a25020ca2338d1d5f97e8e19ee59be485
harupy <17039389+harupy@users.noreply.github.com>
2022-05-28 10:03:36 +0900

    move _log_step_card
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/pipelines/regression/v1/steps/train.py
+++ mlflow/pipelines/regression/v1/steps/train.py
@@ -27,15 +27,6 @@ class TrainStep(BaseStep):
             ".", 1
         )
 
-    def _log_step_card(self, step_name: str):
-        local_card_path = get_step_output_path(
-            pipeline_name=self.pipeline_name,
-            step_name=step_name,
-            relative_path="card.html",
-        )
-        if os.path.exists(local_card_path):
-            mlflow.log_artifact(local_card_path, artifact_path=step_name)
-
     def _run(self, output_directory):
         import pandas as pd
         import numpy as np
