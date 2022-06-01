e0066270b20a3cbfec784c5ed2b3b6b86654b7df
dbczumar <corey.zumar@databricks.com>
2022-06-01 14:37:11 -0700

    Impls
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- mlflow/pipelines/regression/v1/steps/register.py
+++ mlflow/pipelines/regression/v1/steps/register.py
@@ -23,7 +23,6 @@ _logger = logging.getLogger(__name__)
 class RegisterStep(BaseStep):
     def __init__(self, step_config: Dict[str, Any], pipeline_root: str):
         super(RegisterStep, self).__init__(step_config, pipeline_root)
-        self.status = "UNKNOWN"
         self.tracking_config = TrackingConfig.from_dict(step_config)
         self.run_end_time = None
         self.execution_duration = None
@@ -43,72 +42,46 @@ class RegisterStep(BaseStep):
         self.allow_non_validated_model = self.step_config.get("allow_non_validated_model", False)
 
     def _run(self, output_directory):
-        try:
-            run_start_time = time.time()
-            run_id_path = get_step_output_path(
-                pipeline_name=self.pipeline_name,
-                step_name="train",
-                relative_path="run_id",
-            )
-            with open(run_id_path, "r") as f:
-                run_id = f.read()
-
-            model_validation_path = get_step_output_path(
-                pipeline_name=self.pipeline_name,
-                step_name="evaluate",
-                relative_path="model_validation_status",
-            )
-            with open(model_validation_path, "r") as f:
-                model_validation = f.read()
-            artifact_path = "model"
-            if model_validation == "VALIDATED" or (
-                model_validation == "UNKNOWN" and self.allow_non_validated_model
-            ):
-                apply_pipeline_tracking_config(self.tracking_config)
-                # TODO: Figure out how populate self.model_url
-                self.model_uri = "runs:/{run_id}/{artifact_path}".format(
-                    run_id=run_id, artifact_path=artifact_path
-                )
-                self.model_details = mlflow.register_model(
-                    model_uri=self.model_uri,
-                    name=self.register_model_name,
-                    await_registration_for=DEFAULT_AWAIT_MAX_SLEEP_SECONDS,
-                )
-                final_status = self._get_model_version_status(self.model_details.version)
-                self.version = self.model_details.version
-                if final_status == ModelVersionStatus.READY:
-                    self.status = "Done"
-                else:
-                    self.alerts = f"Model failed to register.  Status: {final_status}"
-                    self.status = "Failed"
-            else:
-                self.alerts = (
-                    "Model registration skipped.  Please check the validation "
-                    "result from Evaluate step."
-                )
-                self.status = "Done"
-
-        except Exception:
-            self.status = "Failed"
-            raise
-        finally:
-            self.run_end_time = time.time()
-            self.execution_duration = self.run_end_time - run_start_time
-            try:
-                return self._build_card(output_directory)
-            except Exception as e:
-                # swallow exception raised during building profiles and card.
-                _logger.warning(f"Build card failed: {repr(e)}")
-                # When log level is DEBUG, also log the error stack trace.
-                _logger.debug("", exc_info=True)
-
-    def _get_model_version_status(self, model_version: str) -> ModelVersionStatus:
-        client = MlflowClient()
-        model_version_details = client.get_model_version(
-            name=self.register_model_name,
-            version=model_version,
+        run_start_time = time.time()
+        run_id_path = get_step_output_path(
+            pipeline_name=self.pipeline_name,
+            step_name="train",
+            relative_path="run_id",
         )
-        return ModelVersionStatus.from_string(model_version_details.status)
+        with open(run_id_path, "r") as f:
+            run_id = f.read()
+
+        model_validation_path = get_step_output_path(
+            pipeline_name=self.pipeline_name,
+            step_name="evaluate",
+            relative_path="model_validation_status",
+        )
+        with open(model_validation_path, "r") as f:
+            model_validation = f.read()
+        artifact_path = "model"
+        if model_validation == "VALIDATED" or (
+            model_validation == "UNKNOWN" and self.allow_non_validated_model
+        ):
+            apply_pipeline_tracking_config(self.tracking_config)
+            # TODO: Figure out how populate self.model_url
+            self.model_uri = "runs:/{run_id}/{artifact_path}".format(
+                run_id=run_id, artifact_path=artifact_path
+            )
+            self.model_details = mlflow.register_model(
+                model_uri=self.model_uri,
+                name=self.register_model_name,
+                await_registration_for=DEFAULT_AWAIT_MAX_SLEEP_SECONDS,
+            )
+            self.version = self.model_details.version
+        else:
+            self.alerts = (
+                "Model registration skipped.  Please check the validation "
+                "result from Evaluate step."
+            )
+
+        self.run_end_time = time.time()
+        self.execution_duration = self.run_end_time - run_start_time
+        return self._build_card(output_directory)
 
     def _build_card(self, output_directory: str) -> None:
         from mlflow.pipelines.regression.v1.cards.register import RegisterCard
@@ -130,7 +103,7 @@ class RegisterStep(BaseStep):
             f"**Last run completed at:** `{run_end_datetime.strftime('%Y-%m-%d %H:%M:%S')}`"
         )
         final_markdown.append(f"**Execution duration (s):** `{self.execution_duration:.2f}`")
-        final_markdown.append(f"**Run status:** `{self.status}`")
+        final_markdown.append(f"**Run status:** `{self.get_status(output_directory)}`")
         card.add_markdown("REGISTER_SUMMARY", "<br>\n".join(final_markdown))
         return card
 
