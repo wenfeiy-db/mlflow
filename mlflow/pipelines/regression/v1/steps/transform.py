8bda8c5fa68f887bb4d57624eba83d350c527a84
Sunish Sheth <sunishsheth2009@gmail.com>
2022-06-06 19:45:35 -0700

    Creating a transform step card (#123)
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>

--- mlflow/pipelines/regression/v1/steps/transform.py
+++ mlflow/pipelines/regression/v1/steps/transform.py
@@ -2,6 +2,8 @@ import importlib
 import logging
 import os
 import sys
+import time
+import datetime
 
 import cloudpickle
 
@@ -17,6 +19,8 @@ _logger = logging.getLogger(__name__)
 class TransformStep(BaseStep):
     def __init__(self, step_config, pipeline_root):
         super().__init__(step_config, pipeline_root)
+        self.run_end_time = None
+        self.execution_duration = None
         self.target_col = self.step_config.get("target_col")
         (self.transformer_module_name, self.transformer_method_name,) = self.step_config[
             "transform_method"
@@ -24,6 +28,8 @@ class TransformStep(BaseStep):
 
     def _run(self, output_directory):
         import pandas as pd
+
+        run_start_time = time.time()
 
         train_data_path = get_step_output_path(
             pipeline_name=self.pipeline_name,
@@ -71,7 +77,67 @@ class TransformStep(BaseStep):
             os.path.join(output_directory, "transformed_validation_data.parquet")
         )
 
-        return BaseCard(self.pipeline_name, self.name)
+        self.run_end_time = time.time()
+        self.execution_duration = self.run_end_time - run_start_time
+
+        return self._build_profiles_and_card(
+            train_df, train_transformed, validation_transformed, transformer
+        )
+
+    def _build_profiles_and_card(
+        self, train_df, train_transformed, validation_transformed, transformer
+    ) -> BaseCard:
+        # Build card
+        card = BaseCard(self.pipeline_name, self.name)
+
+        # Build profiles for train_transformed, validation_transformed
+        from pandas_profiling import ProfileReport
+
+        train_transformed_profile = ProfileReport(
+            train_transformed, title="Profile of Train Transformed Dataset", minimal=True
+        )
+        validation_transformed_profile = ProfileReport(
+            validation_transformed, title="Profile of Validation Transformed Dataset", minimal=True
+        )
+        card.add_tab("Data Profile (Train Transformed)", "{{PROFILE}}").add_pandas_profile(
+            "PROFILE", train_transformed_profile
+        )
+        card.add_tab("Data Profile (Validation Transformed)", "{{PROFILE}}").add_pandas_profile(
+            "PROFILE", validation_transformed_profile
+        )
+
+        from sklearn.utils import estimator_html_repr
+        from sklearn import set_config
+
+        set_config(display="diagram")
+        html = estimator_html_repr(transformer)
+        card.add_tab("Pipeline", "{{PIPELINE}}").add_html("PIPELINE", html)
+
+        run_end_datetime = datetime.datetime.fromtimestamp(self.run_end_time)
+        (
+            card.add_tab(
+                f"Run Summary ({self.name.capitalize()})",
+                """
+                {{ RAW_FEATURES_IN_TRANSFORMER }}
+                {{ EXECUTION_DURATION }}
+                {{ RUN_END_TIMESTAMP }}
+                {{ RUN_STATUS }}
+                """,
+            )
+            .add_markdown(
+                "RUN_END_TIMESTAMP",
+                f"**Last run completed at:** `{run_end_datetime.strftime('%Y-%m-%d %H:%M:%S')}`",
+            )
+            .add_markdown(
+                "EXECUTION_DURATION", f"**Execution duration (s):** `{self.execution_duration:.2f}`"
+            )
+            .add_markdown(
+                "RAW_FEATURES_IN_TRANSFORMER",
+                f"**Feature set before transformation:** `{train_df.columns}`",
+            )
+        )
+
+        return card
 
     @classmethod
     def from_pipeline_config(cls, pipeline_config, pipeline_root):
