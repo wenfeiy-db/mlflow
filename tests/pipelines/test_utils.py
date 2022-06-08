d94dc32e5836186e31ee0e0414868020343aa088
jinzhang21 <78067366+jinzhang21@users.noreply.github.com>
2022-06-07 18:36:20 -0700

    Fixed test_util (#133)
    Signed-off-by: jinzhang21 <jin.zhang@databricks.com>

--- tests/pipelines/test_utils.py
+++ tests/pipelines/test_utils.py
@@ -129,6 +129,8 @@ def test_display_html_opens_html_data():
 def test_display_html_opens_html_file(tmp_path):
     html_file = tmp_path / "test.html"
     html_file.write_text("<!DOCTYPE html><html><body><p>Hey</p></body></html>")
-    with mock.patch("subprocess.run") as patched_subprocess:
+    with mock.patch("subprocess.run") as patched_subprocess, mock.patch(
+        "shutil.which", return_value=True
+    ):
         display_html(html_file_path=html_file)
         patched_subprocess.assert_called_once()
